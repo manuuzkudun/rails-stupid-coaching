838745b46f56ec44fbfcb1fc3c152f630294d560
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-02-13 15:26:09 +0000

    coach methods added to the lib folder

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,6 +1,12 @@
+require 'coach'
+
 class CoachingController < ApplicationController
   def answer
-    @question = params[:query] || "no question"
+    @person1 = "You"
+    @question = params[:question] || ""
+
+    @person2 = "Your coach"
+    @answer =  coach_answer_enhanced(@question)
   end
 
   def ask
